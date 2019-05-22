7064edd048c76a1789e65c6ff257f73127f5ad7f
GonzaloIriart <gonzaloiriart@gmail.com>
2019-05-22 19:02:40 -0300

    ex 2

--- training-playground/App.js
+++ training-playground/App.js
@@ -6,16 +6,51 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View
-        style={{
-          flexDirection: 'row',
+        style={{          
           height: 100,
-          padding: 10,
+          flexDirection: "row",
+          padding: 10          
         }}>
-        <View style={{backgroundColor: 'blue',borderColor:'black' ,borderWidth:3.5, flex: 0.3}} />
-        <View style={{backgroundColor: 'green', flex: 0.2}} />         
-      </View>
-      
-     
+        <View style={{backgroundColor: 'pink', flex: 0.3,margin:2}} />
+        <View style={{backgroundColor: 'green', flex: 0.2,margin:2}} /> 
+        <View style={{backgroundColor: 'grey',borderColor:'purple' ,borderWidth:2, flex: 0.7,margin:2}} />  
+        <View style={{backgroundColor: 'red', flex: 0.5,margin:2}} />
+      </View> 
+
+      <View
+        style={{          
+          height: 90,
+          flexDirection: "row",
+          padding: 10          
+        }}>
+        <View style={{backgroundColor: 'blue',borderColor:'orange' ,borderWidth:1.5, flex: 0.7,margin:2}} />
+        <View style={{backgroundColor: 'green', flex: 0.3,margin:2}} /> 
+        <View style={{backgroundColor: 'grey', flex: 0.5,margin:2}} />  
+        <View style={{backgroundColor: 'red', flex: 0.7,margin:2}} />
+      </View> 
+
+      <View
+        style={{          
+          height: 150,
+          flexDirection: "row",
+          padding: 10          
+        }}>
+        <View style={{backgroundColor: 'blue',borderColor:'black' ,borderWidth:3.5, flex: 0.3,margin:2}} />
+        <View style={{backgroundColor: 'green', flex: 0.8,margin:2}} /> 
+        <View style={{backgroundColor: 'grey', flex: 0.2,margin:2}} />  
+        <View style={{backgroundColor: 'red', flex: 0.5,margin:2}} />
+      </View> 
+       
+      <View
+        style={{          
+          height: 15,
+          flexDirection: "row",
+          paddingHorizontal: 10    
+        }}>
+        
+        <View style={{backgroundColor: 'red', flex: 1,margin:2}} />
+      </View> 
+
       </View>
     );
   }
