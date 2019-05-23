1570e7124161022230332966c95ff56388d6201d
GonzaloIriart <gonzaloiriart@gmail.com>
2019-05-22 22:54:05 -0300

    ex 6

--- training-playground/style.js
+++ training-playground/style.js
@@ -1,10 +1,10 @@
-import {StyleSheet} from 'react-native';
+import {Platform,StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
       flexDirection: 'column',
-      backgroundColor: 'blue',
+      backgroundColor :Platform.OS === 'ios' ? 'grey':'green',
       justifyContent: 'space-between',      
     },
     redBox: {
