3c3c859e5e0683b4bb9bae9fe379cdc7584a3ef6
GonzaloIriart <gonzaloiriart@gmail.com>
2019-05-22 21:09:14 -0300

    ex 1

--- training-playground/App.js
+++ training-playground/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
+import {styles} from './style.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.redBox}></View>
+        <View style={styles.purpleBox}></View>
+        <View style={styles.whiteBox}></View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ training-playground/style.js
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'blue',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    redBox: {
+        backgroundColor: 'red',
+        height: 100,
+        width: 100,
+        position: 'absolute',
+        top: 10,
+        left: 10
+    },
+    purpleBox: {
+        backgroundColor: 'purple',
+        height: 200,
+        width: 200,
+        position: 'absolute',
+        top: 120,
+        left: 10
+    },
+    whiteBox: {
+        backgroundColor: 'white',
+        height: 300,
+        width: 300,
+        position: 'absolute',
+        top: 330,
+        left: 10
+    }
+  });
+  
