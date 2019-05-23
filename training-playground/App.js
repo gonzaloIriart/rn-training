4e345fb8e108bfafbc11cf9d8124e1e11b5670c0
GonzaloIriart <gonzaloiriart@gmail.com>
2019-05-23 01:31:34 -0300

    ex 3

--- training-playground/App.js
+++ training-playground/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
+import AppTextInput from './AppTextInput';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: 'enter text' };
-  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({text})}
-        value={this.state.text}
-      />
+      <AppTextInput type = {'password'}></AppTextInput>
       </View>
     );
   }
---
+++ training-playground/AppTextInput.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+
+export default class AppTextInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { text: 'enter text' };
+      }
+
+      onPress = () => {
+        this.setState({text:''});
+      }
+
+    render() {
+        return (
+            <View>
+        <TextInput onTouchStart={this.onPress} clearTextOnFocus={true} secureTextEntry={(this.props.type==='password')} 
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}} 
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
+
+            </View>
+        )
+    }
+}
+
