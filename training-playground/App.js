9a71712159bfbc88185b44f8756056858b7be670
GonzaloIriart <gonzaloiriart@gmail.com>
2019-05-22 23:46:01 -0300

    ex 1

--- training-playground/App.js
+++ training-playground/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'enter text' };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
       </View>
     );
   }
